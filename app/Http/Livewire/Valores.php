2b84893c6bd5307957bdd068f77159779b16b64c
Wilmer Antonio Correa <wcorrea@invertec.cl>
2025-03-19 10:21:56 -0300

    ajustando indicadores economicos

--- app/Http/Livewire/Valores.php
+++ app/Http/Livewire/Valores.php
@@ -4,20 +4,48 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
 class Valores extends Component
 {
     public $uf, $utm, $dolar, $euro;
-    public function mount(){
-        //DATOS DE API
-        $valores = Http::withHeaders([
-            'Accept' => '*/*',
-        ])->get('https://mindicador.cl/api/')->json();
 
-        $this->uf = $valores['uf']['valor'];
-        $this->utm = $valores['utm']['valor'];
-        $this->dolar = $valores['dolar']['valor'];
-        $this->euro = $valores['euro']['valor'];
+    public function mount(){
+        // Intentar obtener los valores de la API con un tiempo de espera de 5 segundos
+        try {
+            $response = Http::withHeaders([
+                'Accept' => '*/*',
+            ])->timeout(5)->get('https://mindicador.cl/api/');
+
+            if ($response->successful()) {
+                $valores = $response->json();
+
+                // Guardar los valores en caché
+                Cache::put('uf', $valores['uf']['valor'], now()->addMinutes(10));
+                Cache::put('utm', $valores['utm']['valor'], now()->addMinutes(10));
+                Cache::put('dolar', $valores['dolar']['valor'], now()->addMinutes(10));
+                Cache::put('euro', $valores['euro']['valor'], now()->addMinutes(10));
+
+                // Asignar los valores a las propiedades
+                $this->uf = $valores['uf']['valor'];
+                $this->utm = $valores['utm']['valor'];
+                $this->dolar = $valores['dolar']['valor'];
+                $this->euro = $valores['euro']['valor'];
+            } else {
+                // Usar los valores en caché si la respuesta no es exitosa
+                $this->useCachedValues();
+            }
+        } catch (\Exception $e) {
+            // Usar los valores en caché si hay una excepción (por ejemplo, tiempo de espera agotado)
+            $this->useCachedValues();
+        }
+    }
+
+    private function useCachedValues() {
+        $this->uf = Cache::get('uf', 'Valor no disponible');
+        $this->utm = Cache::get('utm', 'Valor no disponible');
+        $this->dolar = Cache::get('dolar', 'Valor no disponible');
+        $this->euro = Cache::get('euro', 'Valor no disponible');
     }
 
     public function render(){
--- resources/views/livewire/valores.blade.php
+++ resources/views/livewire/valores.blade.php
@@ -1,26 +1,25 @@
-<!-- COMPONENTE DE LINKS -->
 <div>
-    <div class="card text-dark shadow mt-2" style="height: 19vh; width: 375px; overflow-y: hide; background-color: rgb(255,255,255, 0.95)">
-        <div class="card-header card-valores" style="background-color: rgb(21, 103, 46, 1);">
-            <b style="color: white">Indicadores Económicos</b>
-        </div>
-        <div class="card-body">
-            <div class="row row-top">
-                <div class="col-6">
-                    <p><b>UF:</b> ${{number_format($uf, 2,',','.')}}</p>
-                </div>
-                <div class="col-6">
-                    <p><b>UTM:</b> ${{number_format($utm, 2,',','.')}}</p>
-                </div>
-            <div>
-            <div class="row row-bot">
-                <div class="col-6">
-                    <p><b>DOLAR:</b> ${{number_format($dolar, 2,',','.')}}</p>
-                </div>
-                <div class="col-6">
-                    <p><b>EURO:</b> ${{number_format($euro, 2,',','.')}}</p>
-                </div>
-            <div>
-        </div>
-    </div>
+<div class="card text-dark shadow mt-2" style="height: 19vh; width: 375px; overflow-y: hide; background-color: rgb(255,255,255, 0.95)">
+<div class="card-header card-valores" style="background-color: rgb(21, 103, 46, 1);">
+<b style="color: white">Indicadores Económicos</b>
+</div>
+<div class="card-body">
+<div class="row row-top">
+<div class="col-6">
+<p><b>UF:</b> ${{ number_format(floatval($uf), 2, ',', '.') }}</p>
+</div>
+<div class="col-6">
+<p><b>UTM:</b> ${{ number_format(floatval($utm), 2, ',', '.') }}</p>
+</div>
+</div>
+<div class="row row-bot">
+<div class="col-6">
+<p><b>DOLAR:</b> ${{ number_format(floatval($dolar), 2, ',', '.') }}</p>
+</div>
+<div class="col-6">
+<p><b>EURO:</b> ${{ number_format(floatval($euro), 2, ',', '.') }}</p>
+</div>
+</div>
+</div>
+</div>
 </div>
